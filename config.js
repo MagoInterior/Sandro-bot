4ec2124c5a2b104ec0d94c2d7cb652a172f788b7
MagoInterior <magointerior@gmail.com>
2023-12-13 21:35:11 -0300

    sandro-bot
    ok

---
+++ config.js
@@ -0,0 +1,9 @@
+const { fs } =  require( "fs")
+
+global.donoName = "ᚷ𝐒𝐀𝐍𝐃𝐑𝐎ᚷ" //nome do dono
+global.botName = "𝑺𝑨𝑵𝑫𝑹𝑶-𝑩𝑶𝑻" //nome do bot
+global.donoNumher = "553172595934" //nuemero do dono
+global.botNumber = "553172595934" //nuemro do bot
+global.prefix = "/" //prefixo do bot
+
+// nao apague ""
